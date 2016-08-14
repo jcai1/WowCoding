8c0a3fe937eda9ad8f286186d417b07ed914fbd6
Jeff <jeffreycai@college.harvard.edu>
2016-08-14 17:28:47 -0400

    update BasicTemplate

--- Dev/_BasicTemplate_/_BasicTemplate_.lua
+++ Dev/_BasicTemplate_/_BasicTemplate_.lua
@@ -1,17 +1,18 @@
------ _NAME_HERE_ (Init) -----
+----- _NAME_HERE_ -----
 local A = aura_env
 -- local R = WeakAuras.regions[A.id].region
 -- local S = WeakAurasSaved.displays[A.id]
-local now
-local playerGUID = UnitGUID("player")
+-- local playerGUID = UnitGUID("player")
 
 ----- Set options here -----
-local refreshRate = 10
+local refreshRate = 20
 
------ Custom text -----
+----- Utility -----
+local now
 local refreshInterval = 1 / refreshRate
-local lastRefresh = -999
+local lastRefresh     = -refreshInterval - 1
 local customText
+local shouldShow      = true
 
 local function onCombatEvent(_, subEvent, _, sourceGUID, sourceName, sourceFlags, _,
 	destGUID, destName, destFlags, _, spellID, spellName, _, ...)
@@ -24,7 +25,7 @@ end
 
 local function doCustomText()
 	now = GetTime()
-	if now - lastRefresh > refreshInterval then
+	if now - lastRefresh >= refreshInterval then
 		onRefresh()
 		lastRefresh = now
 	end
@@ -38,6 +39,6 @@ local triggerDispatch = {
 local function doTrigger(event, ...)
 	now = GetTime()
 	triggerDispatch[event](...)
-	return true
+	return shouldShow
 end
 A.doTrigger = doTrigger
