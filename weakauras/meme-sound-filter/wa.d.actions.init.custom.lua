870ec4bf3b779c754aa83553db253e20f2dc29d5
Jeff <magicxcian@gmail.com>
2017-01-31 01:06:56 -0500

    add chat channel to meme-sound-filter

--- weakauras/meme-sound-filter/wa.d.actions.init.custom.lua
+++ weakauras/meme-sound-filter/wa.d.actions.init.custom.lua
@@ -1,13 +1,28 @@
 local A = aura_env
 local R = WeakAuras.regions[A.id].region
 
+local chatChannels = {
+    CHAT_MSG_INSTANCE_CHAT = "Instance",
+    CHAT_MSG_INSTANCE_CHAT_LEADER = "Instance Leader",
+    CHAT_MSG_BN_WHISPER = "BNet Whisper",
+    CHAT_MSG_GUILD = "Guild",
+    CHAT_MSG_PARTY = "Party",
+    CHAT_MSG_PARTY_LEADER = "Party Leader",
+    CHAT_MSG_RAID = "Raid",
+    CHAT_MSG_RAID_LEADER = "Raid Leader",
+    CHAT_MSG_RAID_WARNING = "Raid Warning",
+    CHAT_MSG_SAY = "Say",
+    CHAT_MSG_WHISPER = "Whisper",
+    CHAT_MSG_YELL = "Yell"
+}
+
 local history = {}
 local historyStart = 1
 local historyEnd = 1
 local historyLimit = 1000
 
-local function historyAdd(msg, sender)
-    local line = format("|cff00ffff%s|r [%s]: %s", tostring(date()), tostring(sender) ,tostring(msg))
+local function historyAdd(event, msg, sender)
+    local line = format("|cff00ffff%s|r [%s] [%s]: %s", tostring(date()), tostring(chatChannels[event]), tostring(sender) ,tostring(msg))
     history[historyEnd] = line
     historyEnd = historyEnd + 1
     if historyEnd - historyStart > historyLimit then
@@ -37,11 +52,8 @@ end
 -- copied from EPGP's filter functionality
 local lastMsgID = nil
 local lastMsgFiltered = false
-local function ChatFrameFilter(chatFrame, ...)
-    --local event = select(1, ...)
-    local sender = select(3, ...)
-    local msg = select(2, ...)
-    local msgID = select(12, ...)
+local function ChatFrameFilter(chatFrame, event, msg, sender, ...)
+    local msgID = select(9, ...)
     
     -- Do not process WIM History
     if not msgID or msgID<1 then return end
@@ -54,30 +66,15 @@ local function ChatFrameFilter(chatFrame, ...)
         lastMsgFiltered   = false
         
         if strfind(msg, 'meme sound') or strfind(msg, 'stop memes') then
-            historyAdd(msg, sender)
+            historyAdd(event, msg, sender)
             lastMsgFiltered = true
             return true
         end
     end
 end
 
-local chatChannels = {
-    "CHAT_MSG_INSTANCE_CHAT",
-    "CHAT_MSG_INSTANCE_CHAT_LEADER",
-    "CHAT_MSG_BN_WHISPER",
-    "CHAT_MSG_GUILD",
-    "CHAT_MSG_PARTY",
-    "CHAT_MSG_PARTY_LEADER",
-    "CHAT_MSG_RAID",
-    "CHAT_MSG_RAID_LEADER",
-    "CHAT_MSG_RAID_WARNING",
-    "CHAT_MSG_SAY",
-    "CHAT_MSG_WHISPER",
-    "CHAT_MSG_YELL"
-}
-
 local function init()
-    for _, ch in ipairs(chatChannels) do
+    for ch, _ in pairs(chatChannels) do
         ChatFrame_AddMessageEventFilter(ch, ChatFrameFilter)
     end
 end
