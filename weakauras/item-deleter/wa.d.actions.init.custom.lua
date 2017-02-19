ee66c645772ce68872211d1d5e98e11056c12b76
Jeff <magicxcian@gmail.com>
2017-02-18 19:10:59 -0500

    update item-deleter

--- weakauras/item-deleter/wa.d.actions.init.custom.lua
+++ weakauras/item-deleter/wa.d.actions.init.custom.lua
@@ -1,15 +1,16 @@
 -- Here is where you put the items you want deleted. You can add either the item ID (e.g. 113821) or the item name (e.g. "Battered Iron Horde Helmet"). Examples are provided, feel free to delete them.
 local itemList = {
     42042069,
-    "Gay Ass Item",
+    "Crappy Item",
 }
--- Whether a chat message notification should be displayed
+-- (Advanced) You can add Lua patterns to match names of items to be deleted.
+local namePatterns = {
+}
+-- Whether a chat box notification should be displayed
 -- when this WeakAura deletes an item.
 local bNotify = true
 
 -- Don't touch anything below here unless you know what you're doing.
-
-local strformat = string.format
 
 local A = aura_env
 local t0 = 0
@@ -24,6 +25,15 @@ for i = 1, #itemList do
     else
         itemNameTest[item] = true
     end
+end
+
+local function matchesAnyNamePattern(name)
+    for i = 1, #namePatterns do
+        if strmatch(name, namePatterns[i]) then
+            return true
+        end
+    end
+    return false
 end
 
 -- Does sweep for bag i
@@ -45,12 +55,12 @@ local function sweep(i)
         local id = GetContainerItemID(i, j)
         if id then
             local name = GetItemInfo(id)
-            if itemIDTest[id] or itemNameTest[name] then
+            if itemIDTest[id] or itemNameTest[name] or matchesAnyNamePattern(name) then
                 PickupContainerItem(i, j)
                 DeleteCursorItem()
                 ClearCursor()
                 if bNotify then
-                    print(strformat(
+                    print(format(
                             "|cffffff00Item deleted:|r %s (itemID %d)",
                             tostring(name), id))
                 end
